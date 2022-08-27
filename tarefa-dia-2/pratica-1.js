33484881d0f99341a36db1a1d3b489ec849a4496
LudmilaGodoy <luds.godoy@gmail.com>
2022-08-27 10:49:46 -0300

    texto daa pratica 1

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -4,14 +4,18 @@ let profissao
 let idade
 let lugar
 
-nome = "ludmila"
+nome = "Ludmila"
 email = "luds.godoy@gmail.com"
 profissao = "progrmadora"
-idade = "22"
-lugar = "bh"
+idade = "22 anos"
+lugar = "Kansas"
 
 console.log(nome)
 console.log(email)
 console.log(profissao)
 console.log(idade)
 console.log(lugar)
+
+let mensagem = "Olá, " + nome + "! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para "+ lugar +". Confirma pra gente alguns dados? A sua idade é "+ idade + ", você é " +profissao + " e o seu email é "+ email + "? Ficamos no aguardo. Muito obrigado e boa viagem! :D"
+
+console.log(mensagem)
