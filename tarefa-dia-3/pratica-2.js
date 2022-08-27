604ac4373f41310462e34ed9f1e243c2aecf9857
LudmilaGodoy <luds.godoy@gmail.com>
2022-08-27 18:20:02 -0300

    multiplicando atraves da programacao

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,22 @@
+let multiplicador = 9
+let n = 9
+let i = 1
+
+
+while (i<=9) {
+    let resultado = multiplicador * i
+    let valor = `${multiplicador} X ${i} = ${resultado}`
+    console.log(valor)
+    i = i + 1 // ou i++ pra tipo encerrar o loop (ele vai tipo somando 1 mo i que vai alterando o valor dele, ate alcancar o n = 9)
+}
+
+
+
+
+
+
+//while (i <= 9) {
+  //  let resultado = i*multiplicador
+    //let valorExibido = `${multiplicador} X ${i} = ${resultado}`
+  //  console.log(valorExibido)
+//} //9x1=9
