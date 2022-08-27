4df85460763ea2a2ec8716054e5d676f0366f5c0
LudmilaGodoy <luds.godoy@gmail.com>
2022-08-26 21:32:42 -0300

    continuando

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -9,3 +9,9 @@ email = "luds.godoy@gmail.com"
 profissao = "progrmadora"
 idade = "22"
 lugar = "bh"
+
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(lugar)
